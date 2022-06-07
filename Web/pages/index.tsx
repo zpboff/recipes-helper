f308ebf743cd14845ac6068c0f348460a10c8161
p.zakirov <zakirov@cmsplanet.ru>
2022-06-07 15:19:30 +0300

    Layout structure

--- Web/pages/index.tsx
+++ Web/pages/index.tsx
@@ -1,29 +1,30 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-        <Head>
-            <title>Create Next App</title>
-            <meta name="description" content="Recipes helper" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>      
-        <header>
-            Заголовок
-        </header>
-        <aside>
-            Меню
-        </aside>
-        <main>
-            Контент
-        </main>
-        <footer>
-            Подвал
-        </footer>
-    </div>
-  )
+    return (
+        <div className={styles.app}>
+            <Head>
+                <title>Create Next App</title>
+                <meta name="description" content="Recipes helper"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <header className={styles.header}>
+                Заголовок
+            </header>
+            <aside className={styles.aside}>
+                Меню
+            </aside>
+            <main className={styles.main}>
+                Контент
+            </main>
+            <footer className={styles.footer}>
+                Подвал
+            </footer>
+        </div>
+    )
 }
 
 export default Home
