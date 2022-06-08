63456739009a13a8618e4c4c261f91691784b87b
p.zakirov <zakirov@cmsplanet.ru>
2022-06-08 16:21:04 +0300

    Login page markup

--- Web/components/Link/index.tsx
+++ Web/components/Link/index.tsx
@@ -1,16 +1,15 @@
 ﻿import React from 'react';
 import styles from './Link.module.css';
 
-type Props = React.PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement> & {
-    
+type Props = React.PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement> & {    
 }>;
 
-const Link: React.FC<Props> = ({ children, className = "", ...linkProps }) => {
+const Link: React.FC<Props> = React.forwardRef<HTMLAnchorElement, Props>(({ children, className = "", ...linkProps }, ref) => {
     return (
-        <a {...linkProps} className={`${styles.link} ${className}`}>
+        <a {...linkProps} className={`${styles.link} ${className}`} ref={ref}>
             {children}
         </a>
     );
-};
+});
 
 export {Link}
