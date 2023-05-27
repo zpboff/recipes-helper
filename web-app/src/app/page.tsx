398b2fa3c85c9bca835c7d827788112628c45d44
zakirov.p <zakirov.p@skbkontur.ru>
2023-05-27 22:26:26 +0300

    Basic page structure

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,6 +1,13 @@
+import styles from './page.module.css';
+
+export const metadata = {
+    title: 'Главная - Подбор рецептов',
+    description: 'Поиск рецептов по ингредиентам',
+}
+
 export default function Home() {
   return (
-    <main>
+    <main className={styles.page}>
       Main
     </main>
   )
