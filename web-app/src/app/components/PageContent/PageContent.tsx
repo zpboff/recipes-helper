e40a1935848ce2128c1b8999492fc20f099be637
zakirov.p <zakirov.p@skbkontur.ru>
2023-05-27 23:26:20 +0300

    Temporal styles removing

---
+++ web-app/src/app/components/PageContent/PageContent.tsx
@@ -0,0 +1,14 @@
+﻿import { SideMenu } from "./SideMenu";
+import styles from "./PageContent.module.scss";
+import { PropsWithChildren } from "react";
+
+export function PageContent({ children }: PropsWithChildren) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.grid}>
+                <SideMenu />
+                <main className={styles.content}>{children}</main>    
+            </div>            
+        </div>
+    );
+}
