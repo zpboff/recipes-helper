02e0b0e87b9741196fce773a2d2ae8ed27df7468
zakirov.p <zakirov.p@skbkontur.ru>
2023-09-08 21:53:40 +0300

    Make elastic client singleton-like

--- Core/Core.Elastic/ElasticClientFactory.cs
+++ Core/Core.Elastic/ElasticClientFactory.cs
@@ -1,15 +1,27 @@
-﻿using Elastic.Clients.Elasticsearch;
+﻿using System.Collections.Concurrent;
+using Elastic.Clients.Elasticsearch;
 using Elastic.Transport;
 
 namespace Core.Elastic;
 
 public class ElasticClientFactory : IElasticClientFactory
 {
+    private readonly ConcurrentDictionary<string, ElasticsearchClient> _clients = new();
+
     public ElasticsearchClient GetClient(IElasticSettings settings)
     {
+        if (_clients.TryGetValue(settings.ConnectionString, out var createdClient))
+        {
+            return createdClient;
+        }
+        
         var clientSettings = new ElasticsearchClientSettings(new Uri(settings.ConnectionString))
             .Authentication(new BasicAuthentication(settings.User, settings.Password));
 
-        return new ElasticsearchClient(clientSettings);
+        var client = new ElasticsearchClient(clientSettings);
+
+        _clients.TryAdd(settings.ConnectionString, client);
+
+        return client;
     }
 }
