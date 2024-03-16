1b93e14f2d290350ea80ddb1d6f0bf53a0b8d637
zpb_off <>
2024-03-16 23:55:42 +0300

    Remove overengineered projects

--- BFF/BFF.Web/Providers/IRecipesSearchProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using Recipes.API.Models.Shared;
-
-namespace BFF.Web.Providers;
-
-public interface IRecipesSearchProvider
-{
-    Task<IEnumerable<RecipeReadDto>?> Search(string query);
-}
--- BFF/BFF.Web/Settings/RecipesApiSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Settings;
-
-namespace BFF.Web.Settings;
-
-public class RecipesApiSettings: ISettings
-{
-    public string Url { get; set; }
-    public string Version { get; set; }
-}
